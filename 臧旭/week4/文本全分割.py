973bf612506d4f58107301dff41e12a13f4548d1
zhuyiliulian <1240991569@qq.com>
2024-07-12 21:46:13 +0800

    文本全分割实现

---
+++ 臧旭/week4/文本全分割.py
@@ -0,0 +1,31 @@
+def all_segmentations(sentence, dictionary):
+    def segment(sentence, dictionary, memo):
+        if sentence in memo:
+            return memo[sentence]
+
+        result = []
+        if sentence in dictionary:
+            result.append([sentence])
+
+        for i in range(1, len(sentence)):
+            prefix = sentence[:i]
+            if prefix in dictionary:
+                suffix = sentence[i:]
+                suffix_segmentations = segment(suffix, dictionary, memo)
+                for segmentation in suffix_segmentations:
+                    result.append([prefix] + segmentation)
+
+        memo[sentence] = result
+        return result
+    memo = {}
+    return segment(sentence, dictionary, memo)
+
+
+# 示例
+dictionary = {"分", "切", "全", "切分", "按", "键", "进行", "按键", "全切分"}
+Dict = {"经常", "经", "有", "常", "有意见", "歧", "意见", "分歧", "见", "意", "见分歧", "分"}
+sentence = "经常有意见分歧"
+
+segmentations = all_segmentations(sentence, Dict)
+result = sorted(segmentations, key=lambda x: len(x))
+print(result)
