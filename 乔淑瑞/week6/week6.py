3347657e649284dede5eda7ff348b11fd8c692dc
qiaoshurui <shurui.qiao@seetacloud.com>
2024-08-01 11:49:51 +0800

    第六周作业修改

--- 乔淑瑞/week6/week6.py
+++ 乔淑瑞/week6/week6.py
@@ -1,15 +1,26 @@
-"""
+n = 2  # 输入最大句子个数
+vocab = 21128  # 词表数目
+max_sequence_length = 512  # 最大句子长度
+embedding_size = 768  # embedding维度
+hide_size = 3072  # 隐藏层维数
+
 # Embedding
-# token=21128*768
-# segment=2*768
-# position=512*768
+embedding_parameters = max_sequence_length * embedding_size + n * embedding_size + max_sequence_length * embedding_size
+# lay_norm:embedding_size + embedding_sizes
+embedding_parameters += embedding_size + embedding_size
 
-# 多头机制
-# h*h--->h*h/12-->768*(768/12)*3*12
-# 过一个线性层-->768*768
-# Feed-Forward 前馈神经网络，两个线性层
-# 768×(768*4)+3072   (l*h)*(h*4h)-->l*4h
-# (768*4)×768+768    l*4h * 4h*h
+# self_attention:*3分别是Q,K,V,3个线性层
+self_attention_parameters = (embedding_size * embedding_size + embedding_size) * 3
+# 一个线性层+一个lay_norm
+self_attention_out_parameters = embedding_size * embedding_size + embedding_size + embedding_size + embedding_size
 
-# *12
-"""
+# Feed-Forward:前馈神经网络，两个线性层，一个lay_norm
+feed_forward_parameters = embedding_size * hide_size + hide_size + embedding_size * hide_size + embedding_size + embedding_size + embedding_size
+
+# pool_fc层参数,使用第一个向量
+pool_fc_parameters = embedding_size * embedding_size + embedding_size
+
+# 总参数数目
+total_parameters = embedding_parameters + self_attention_parameters + self_attention_out_parameters + feed_forward_parameters + pool_fc_parameters
+
+print("Total parameters:", total_parameters)
