00f3d1961f8ee582bd1e7aae513976c047f88e9b
Swarson <1316104304@qq.com>
2024-08-03 00:54:50 +0800

    Add files via upload

---
+++ 宋新宇/week7/nn_pipline/config.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+配置参数信息
+"""
+
+Config = {
+    "model_path": "output",
+    "train_data_path": "../data/train_data.csv",
+    "valid_data_path": "../data/test_data.csv",
+    "vocab_path":"chars.txt",
+    "model_type":"bert",
+    "max_length": 30,
+    "hidden_size": 256,
+    "kernel_size": 3,
+    "num_layers": 2,
+    "epoch": 15,
+    "batch_size": 128,
+    "pooling_style":"max",
+    "optimizer": "adam",
+    "learning_rate": 1e-3,
+    "pretrain_model_path":r"E:\八斗学院AI课程\NLP\八斗精品班\第六周 预训练模型\bert-base-chinese\bert-base-chinese",
+    "seed": 987
+}
+
