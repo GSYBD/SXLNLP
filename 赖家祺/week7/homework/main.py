7f236f1c2ee68560d9407af096caf34a67aee2a0
my_Raizo <79581103+githubRaizo@users.noreply.github.com>
2024-08-05 00:33:50 +0800

    Add files via upload

---
+++ 赖家祺/week7/homework/main.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+import pandas as pd
+import torch
+import os
+import random
+import os
+import numpy as np
+import logging
+from config import Config
+from model import TorchModel, choose_optimizer
+from evaluate import Evaluator
+from loader import load_data
+#[DEBUG, INFO, WARNING, ERROR, CRITICAL]
+logging.basicConfig(level=logging.INFO, format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+"""
+模型训练主程序
+"""
+
+
+seed = Config["seed"] 
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+
+def main(config):
+    #创建保存模型的目录
+    if not os.path.isdir(config["model_path"]):
+        os.mkdir(config["model_path"])
+    #加载训练数据
+    train_data = load_data(config["train_data_path"], config)
+    #加载模型
+    model = TorchModel(config)
+    # 标识是否使用gpu
+    cuda_flag = torch.cuda.is_available()
+    if cuda_flag:
+        logger.info("gpu可以使用，迁移模型至gpu")
+        model = model.cuda()
+    #加载优化器
+    optimizer = choose_optimizer(config, model)
+    #加载效果测试类
+    evaluator = Evaluator(config, model, logger)
+    #训练
+    for epoch in range(config["epoch"]):
+        epoch += 1
+        model.train()
+        # logger.info("epoch %d begin" % epoch)
+        train_loss = []
+        for index, batch_data in enumerate(train_data):
+            if cuda_flag:
+                batch_data = [d.cuda() for d in batch_data]
+
+            optimizer.zero_grad()
+            # print(batch_data)
+            input_ids, labels = batch_data   #输入变化时这里需要修改，比如多输入，多输出的情况
+            loss = model.forward(input_ids, labels)
+            loss.backward()
+            optimizer.step()
+
+            train_loss.append(loss.item())
+            # if index % int(len(train_data) / 2) == 0:
+                # logger.info("batch loss %f" % loss)
+        # logger.info("epoch average loss: %f" % np.mean(train_loss))
+        acc = evaluator.eval(epoch)
+    # model_path = os.path.join(config["model_path"], "epoch_%d.pth" % epoch)
+    # torch.save(model.state_dict(), model_path)  #保存模型权重
+    return acc
+
+
+if __name__ == "__main__":
+    # main(Config)
+
+    # for model in ["cnn"]:
+    #     Config["model_type"] = model
+    #     print("最后一轮准确率：", main(Config), "当前配置：", Config["model_type"])
+
+    #对比所有模型
+    #中间日志可以关掉，避免输出过多信息
+    # 超参数的网格搜索
+    results = pd.DataFrame(columns=['Model', 'learning rate', 'hidden size', 'batch size', 'pooling', 'accuracy'])
+    # print(results)
+    for model in ["fast_text", "gated_cnn", "rnn"]:
+        Config["model_type"] = model
+        start = datetime.now()
+
+        for lr in [1e-3, 1e-4]:
+            Config["learning_rate"] = lr
+            for hidden_size in [128]:
+                Config["hidden_size"] = hidden_size
+                for batch_size in [64]:
+                    Config["batch_size"] = batch_size
+                    for pooling_style in ["avg", "max"]:
+                        Config["pooling_style"] = pooling_style
+                        acc = main(Config)
+                        # print("最后一轮准确率：", acc, "当前配置：", Config)
+                        res = {'Model': model, 'learning rate':lr, 'hidden size':hidden_size, 'batch size':batch_size, 'pooling':pooling_style, 'accuracy':acc}
+                        results = pd.concat([results, pd.DataFrame([res])], ignore_index=True)
+                        # print(results)
+        end = datetime.now()
+        print(f"{model} time cost: {(end-start).seconds}")
+    print("\nfinal csv done...")
+    results.to_csv('output/result.csv')
