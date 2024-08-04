f6b6d3329b020ddab8ce58941776788236c9ec03
Serena9 <51285364+Serena9@users.noreply.github.com>
2024-08-04 20:35:53 +0800

    Update loader.py

--- 蒋烨/week7/loader.py
+++ 蒋烨/week7/loader.py
@@ -16,11 +16,6 @@ class DataGenerator:
     def __init__(self, data_path, config):
         self.config = config
         self.path = data_path
-        # self.index_to_label = {0: '家居', 1: '房产', 2: '股票', 3: '社会', 4: '文化',
-        #                        5: '国际', 6: '教育', 7: '军事', 8: '彩票', 9: '旅游',
-        #                        10: '体育', 11: '科技', 12: '汽车', 13: '健康',
-        #                        14: '娱乐', 15: '财经', 16: '时尚', 17: '游戏'}
-        # self.label_to_index = dict((y, x) for x, y in self.index_to_label.items())
         self.config["class_num"] = 2
         if self.config["model_type"] == "bert":
             self.tokenizer = BertTokenizer.from_pretrained(config["pretrain_model_path"])
