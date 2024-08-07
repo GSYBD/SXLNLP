ff65d2c7d2b5ac7b53778a680802b70d706213f5
qiaoshurui <shurui.qiao@seetacloud.com>
2024-08-07 19:01:45 +0800

    代码修改

--- 乔淑瑞/week7/config.py
+++ 乔淑瑞/week7/config.py
@@ -6,19 +6,19 @@
 
 Config = {
     "model_path": "output",
-    "train_data_path": "/data/train.csv",
-    "valid_data_path": "/data/test.csv",
+    "train_data_path": "data/train_data.txt",
+    "valid_data_path": "data/valid_data.txt",
     "vocab_path": "chars.txt",
     "model_type": "bert",
     "max_length": 30,
     "hidden_size": 128,
     "kernel_size": 3,
     "num_layers": 2,
-    "epoch": 15,
+    "epoch": 3,
     "batch_size": 128,
     "pooling_style": "max",
     "optimizer": "adam",
-    "test_size": 0.3,
+    "test_size": 0.2,
     "learning_rate": 1e-3,
     "pretrain_model_path": r"D:\material\八斗\第六周 预训练模型\bert-base-chinese",
     "seed": 987
--- 乔淑瑞/week7/loader.py
+++ 乔淑瑞/week7/loader.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import json
+import re
+import os
 import torch
+import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from transformers import BertTokenizer
-from config import Config
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
 """
 数据加载
 """
 
 
 class DataGenerator:
-    def __init__(self, data, config):
+    def __init__(self, data_path, config):
         self.config = config
-        self.data = data
-        self.index_to_label = {0: "负面评价", 1: "正面评价"}
+        self.path = data_path
+        self.index_to_label = {0: '差评', 1: '好评'}
         self.label_to_index = dict((y, x) for x, y in self.index_to_label.items())
         self.config["class_num"] = len(self.index_to_label)
         if self.config["model_type"] == "bert":
@@ -26,17 +25,20 @@ class DataGenerator:
         self.config["vocab_size"] = len(self.vocab)
         self.load()
 
+
     def load(self):
         self.data = []
         with open(self.path, encoding="utf8") as f:
             for line in f:
-                line = json.loads(line)
-                tag = line["tag"]
-                label = self.label_to_index[tag]
-                title = line["title"]
+                if line.startswith("0,"):
+                    label = 0
+                elif line.startswith("1,"):
+                    label = 1
+                else:
+                    continue
+                title = line[2:].strip()
                 if self.config["model_type"] == "bert":
-                    input_id = self.tokenizer.encode(title, max_length=self.config["max_length"],
-                                                     pad_to_max_length=True)
+                    input_id = self.tokenizer.encode(title, max_length=self.config["max_length"], pad_to_max_length=True)
                 else:
                     input_id = self.encode_sentence(title)
                 input_id = torch.LongTensor(input_id)
@@ -51,7 +53,7 @@ class DataGenerator:
         input_id = self.padding(input_id)
         return input_id
 
-    # 补齐或截断输入的序列，使其可以在一个batch内运算
+    #补齐或截断输入的序列，使其可以在一个batch内运算
     def padding(self, input_id):
         input_id = input_id[:self.config["max_length"]]
         input_id += [0] * (self.config["max_length"] - len(input_id))
@@ -63,36 +65,22 @@ class DataGenerator:
     def __getitem__(self, index):
         return self.data[index]
 
-
 def load_vocab(vocab_path):
     token_dict = {}
     with open(vocab_path, encoding="utf8") as f:
         for index, line in enumerate(f):
             token = line.strip()
-            token_dict[token] = index + 1  # 0留给padding位置，所以从1开始
+            token_dict[token] = index + 1  #0留给padding位置，所以从1开始
     return token_dict
 
 
-# 用torch自带的DataLoader类封装数据
+#用torch自带的DataLoader类封装数据
 def load_data(data_path, config, shuffle=True):
     dg = DataGenerator(data_path, config)
     dl = DataLoader(dg, batch_size=config["batch_size"], shuffle=shuffle)
     return dl
 
-
-def load_data(data_path, config, shuffle=True):
-    df = pd.read_csv(data_path, encoding="utf8")
-    data = [(int(row.iloc[0]), row.iloc[1]) for _, row in df.iterrows()]
-    train_data, test_data = train_test_split(data, test_size=config["test_size"], random_state=config["random_state"], shuffle=True)
-
-    train_dataset = DataGenerator(train_data, config)
-    test_dataset = DataGenerator(test_data, config)
-
-    train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=shuffle)
-    test_loader = DataLoader(test_dataset, batch_size=config["batch_size"], shuffle=False)
-    return train_loader, test_loader
-
 if __name__ == "__main__":
-
+    from config import Config
     dg = DataGenerator("valid_tag_news.json", Config)
     print(dg[1])
