b66eec192944da70d501e207b5ba388be67cc4db
aiyaohu <45875855+aiyaohu@users.noreply.github.com>
2024-08-16 00:24:52 +0800

    第九周作业 -- 使用bert实现NER

---
+++ 胡超/week9/config.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+配置参数信息
+"""
+
+Config = {
+    "model_path": "model_output",
+    "schema_path": "ner_data/schema.json",
+    "train_data_path": "ner_data/train",
+    "valid_data_path": "ner_data/test",
+    "vocab_path": "chars.txt",
+    "model_type": "bert",
+    "max_length": 100,
+    "hidden_size": 256,
+    "num_layers": 3,
+    "epoch": 20,
+    "batch_size": 16,
+    "optimizer": "adam",
+    "learning_rate": 1e-4,
+    "use_crf": True,
+    "class_num": 9,
+    "bert_path": r"./bert-base-chinese"
+}
