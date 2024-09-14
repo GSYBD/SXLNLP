49a3dc7cc3a60569eba1c919a246f60228af9061
lh <huhecheng@happay.cn>
2024-09-15 00:18:59 +0800

    13周作业

---
+++ LiuHong/week13/config.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+配置参数信息
+"""
+
+Config = {
+    "model_path": "model_output",
+    "schema_path": "ner_data/schema.json",
+    "train_data_path": "ner_data/train",
+    "valid_data_path": "ner_data/test",
+    "vocab_path":"chars.txt",
+    "max_length": 100,
+    "hidden_size": 768,
+    "num_layers": 3,
+    "epoch": 20,
+    "batch_size": 16,
+    "optimizer": "adam",
+    "learning_rate": 1e-4,
+    "use_crf": False,
+    "class_num": 9,
+    "bert_path": r"/Users/liuhong/PycharmProjects/nlp/week6/bert-base-chinese",
+    "tuning_tactics":"lora_tuning"
+}
